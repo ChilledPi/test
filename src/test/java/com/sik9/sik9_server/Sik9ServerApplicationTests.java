d396fc1195c441372bf1a10b2231ad22905f4ab4
ChilledPi <chilledpi7@gmail.com>
2025-08-12 14:29:51 +0900

    init

---
+++ src/test/java/com/sik9/sik9_server/Sik9ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sik9.sik9_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sik9ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
