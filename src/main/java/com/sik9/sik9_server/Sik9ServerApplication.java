8e71a4b223d043ea851a3d4bb00b597e3fe71876
ChilledPi <chilledpi7@gmail.com>
2025-08-12 16:07:27 +0900

    test

---
+++ src/main/java/com/sik9/sik9_server/Sik9ServerApplication.java
@@ -0,0 +1,12 @@
+package com.sik9.sik9_server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sik9ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sik9ServerApplication.class, args);
+    }
+
+}
